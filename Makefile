4e24fb91d9d288445fdb6eaae7fd32eff254d1b1
mayhl <michaelangelo.yh.lam@gmail.com>
2023-07-13 17:04:01 -0500

    Add script to clone or soft link external repos

--- Makefile
+++ Makefile
@@ -1,12 +1,29 @@
 # Minimal makefile for Sphinx documentation
 #
 
+# Git repo urls
+# NOTE: HTTPS and SSH are considered to be different
+FUNWAVE_REPO="https://github.com/fengyanshi/FUNWAVE-TVD.git"
+FUNTOOL_REPO="git@github.com:mayhl/FUNWAVE-TVD-Python-Tools.git"
+
+# Optional paths to local repos to use instead of cloning 
+# NOTE: HTTPS and SSH are considered to be different; therefore,
+#       ensure above urls match urls at paths 
+#       (e.g. 'git remote get-url origin' while in repo path)  
+FUNWAVE_PATH=
+FUNTOOL_PATH=
+
 # You can set these variables from the command line.
 SPHINXOPTS    =
 SPHINXBUILD   = sphinx-build
 SPHINXPROJ    = funwave
-SOURCEDIR     = .
+SOURCEDIR     = src
 BUILDDIR      = build
+
+# Paths to 
+EPATH=external
+FUNWAVE_EPATH= $(EPATH)/FUNWAVE
+FUNTOOL_EPATH= $(EPATH)/FUNTOOL
 
 # Put it first so that "make" without argument is like "make help".
 help:
@@ -17,4 +34,6 @@ help:
 # Catch-all target: route all unknown targets to Sphinx using the new
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
 %: Makefile
+	@sh scripts/link_repos.sh ${FUNWAVE_REPO} ${FUNWAVE_EPATH} ${FUNWAVE_PATH}
+	@sh scripts/link_repos.sh ${FUNTOOL_REPO} ${FUNTOOL_EPATH} ${FUNTOOL_PATH}
 	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
---
+++ scripts/link_repos.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Git repo link
+# NOTE: HTTPS and SSH links to the same repo 
+#       are considered different repos 
+REPO=$1
+# Path to internal repo to create
+IPATH=$2
+# Optional path to external repo to create soft link too
+EPATH=$3
+
+
+# Function for checking if repo at path is a valid repo
+check_repo() {
+
+	RPATH=$1	
+	REPO=$2
+
+	# Checking if directory at path is a git repo
+	if ! [ -d "${RPATH}/.git" ]; then
+		echo "FATAL: Path '$RPATH' is not a git repo!!!"
+		exit 1
+	fi
+
+	# Checking repos at path is same as specified
+	CDIR=$(pwd)
+	cd ${RPATH} 
+	REPO_AT_PATH=$(git remote get-url origin)
+	if ! [[ ${REPO} == ${REPO_AT_PATH} ]]; then
+		echo "FATAL: Repo at path '${RPATH}' does not match specified repo!!! Expected '${REPO}', got '${REPO_AT_PATH}'."
+		exit 1
+	fi
+	cd ${CDIR}	
+
+
+	# Checking if repo is up-to-date
+	STATUS=$(git status -uno)
+	if ! [[ ${STATUS} == *"Your branch is up to date"* ]]; then
+		echo "Local repo is not up to date. Do you to pull latest update [Y/N]?"
+		read UPDATE
+
+		if [[ "${UPDATE}" == "y" || "${UPDATE}" == "Y" ]]; then
+			echo "Fetching latest repo version"
+			git fetch origin
+			git pull
+		elif [[ "${UPDATE}" == "n" || "${UPDATE}" == "N" ]]; then
+			echo "Using current repo version"
+		else
+			echo "FATAL: Invalid response!!!"
+			exit 1
+		fi
+
+		exit 0
+	fi
+}
+
+echo "Checking repo '${REPO}'"
+
+# Checking if internal path exists and if it does, check if path is a git repo
+if [ -d "${IPATH}" ]; then
+	check_repo ${IPATH} ${REPO}
+fi
+
+# If internal path does not exist, cloning repo or create soft link to external repo 
+if [[ ${EPATH} == "" ]]; then
+	if ! [ -d "${IPATH}" ]; then
+		echo "Cloning repo"
+		git clone ${REPO} ${IPATH}
+	fi
+else
+	echo "Creating soft link to local repo"
+	check_repo $EPATH ${REPO}
+	ln -s ${EPATH} ${IPATH}	
+fi
